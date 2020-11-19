c84386f13196640c97023eed67d0eae49be2fd68
hannahbish <510894@chrome.escg.ac.uk>
2020-11-19 16:15:39 +0000

    Coding

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -9,7 +9,7 @@ public class Player : MonoBehaviour
     public float timeToJumpApex = .4f;
     float accelerationTimeAirborne = .2f;
     float accelerationTimeGrounded = .1f;
-    float moveSpeed = 6;
+    float moveSpeed = 9;
 
     float gravity;
     float jumpVelocity;
